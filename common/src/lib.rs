f838652e5c62be169744b50ba917aeb68c0ba3cf
Einfach_Irgendwa1 <faris.karge@berlin>
2024-11-19 17:10:49 +0100

    wrote a working tokenizer first try

---
+++ common/src/lib.rs
@@ -0,0 +1,39 @@
+use colored::Colorize;
+use log::{set_logger, set_max_level, Level, LevelFilter, Log, SetLoggerError};
+
+pub struct Logger {}
+
+impl Log for Logger {
+    fn enabled(&self, _: &log::Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &log::Record) {
+        if self.enabled(record.metadata()) {
+            println!(
+                "{}",
+                match record.level() {
+                    Level::Error => format!("[ERROR] {}", record.args()).red(),
+                    Level::Warn => format!("[WARN ] {}", record.args()).yellow(),
+                    Level::Info => format!("[INFO ] {}", record.args()).cyan(),
+                    Level::Debug => format!("[DEBUG] {}", record.args()).green(),
+                    Level::Trace => format!("[TRACE] {}", record.args()).black(),
+                }
+            );
+        }
+    }
+
+    fn flush(&self) {}
+}
+
+pub fn use_recommended_logger(_level_filter: LevelFilter) -> Result<(), SetLoggerError> {
+    set_logger(&Logger {})?;
+
+    #[cfg(debug_assertions)]
+    set_max_level(LevelFilter::Debug);
+
+    #[cfg(not(debug_assertions))]
+    set_max_level(_level_filter);
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,119 @@
+use std::{fs::File, io::Read, str::Chars};
+
+use clap::Parser;
+use eyre::{Context, Result};
+use log::{info, warn};
+
+#[derive(Parser)]
+struct Cli {
+    input: String,
+
+    #[arg(short, long, global = true)]
+    output: Option<String>,
+}
+
+#[derive(Debug)]
+enum Token {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    LParen,
+    RParen,
+    Int(i32),
+    Ident(String),
+}
+
+fn main() -> Result<()> {
+    let Cli { input, output } = Cli::parse();
+
+    if !input.ends_with(".tl") {
+        warn!("Input file `{input}` doesn't end in `.tl`. It should do that.")
+    }
+
+    let output = output.unwrap_or_else(|| {
+        let mut chars = input.chars().rev().skip_while(|c| *c != '.');
+        if !chars.next().is_some() {
+            let output = format!("{input}.out");
+            warn!("Input file `input` doesn't have a file extension.");
+            warn!("Renaming output to {output} to not override input file.");
+            return output;
+        } else {
+            chars.collect()
+        }
+    });
+
+    info!("{input} --- tlc ---> {output}");
+
+    let mut input_file =
+        File::open(&input).wrap_err_with(|| format!("Failed to open `{input}`."))?;
+
+    let mut content = String::new();
+    input_file
+        .read_to_string(&mut content)
+        .wrap_err_with(|| format!("Failed to read from `{input}`."))?;
+
+    let tokens = dbg!(tokenize(&content));
+
+    Ok(())
+}
+
+struct SkipWhitespace<'a>(Chars<'a>);
+
+impl<'a> Iterator for SkipWhitespace<'a> {
+    type Item = <Chars<'a> as Iterator>::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.0.next()? {
+            ' ' => self.next(),
+            x => Some(x),
+        }
+    }
+}
+
+fn is_digit(c: &char) -> bool {
+    c.is_digit(10)
+}
+
+fn is_normal_character(c: &char) -> bool {
+    c.is_digit(36)
+}
+
+fn build_from_and_while(
+    c: &char,
+    i: &mut impl Iterator<Item = char>,
+    f: impl Fn(&char) -> bool,
+) -> String {
+    let mut out = c.to_string();
+    out.extend(i.by_ref().take_while(f));
+    out
+}
+
+fn tokenize(input: &str) -> Result<Vec<Token>> {
+    let mut tokens = Vec::new();
+    let mut input = SkipWhitespace(input.chars());
+
+    while let Some(next) = input.next() {
+        let token = match next {
+            '+' => Token::Add,
+            '-' => Token::Sub,
+            '*' => Token::Mul,
+            '/' => Token::Div,
+            '(' => Token::LParen,
+            ')' => Token::RParen,
+            ref x if is_digit(x) => Token::Int(
+                build_from_and_while(x, &mut input, is_digit)
+                    .parse()
+                    .unwrap(),
+            ),
+            ref x if is_normal_character(x) => {
+                Token::Ident(build_from_and_while(x, &mut input, is_normal_character))
+            }
+            ref x => Err(eyre::eyre!("Found an invalid character: {x}"))?,
+        };
+
+        tokens.push(token);
+    }
+
+    Ok(tokens)
+}
