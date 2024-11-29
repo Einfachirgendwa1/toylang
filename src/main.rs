1f6d9203d28335d2d683b66816b83e85c3d6bcfe
Einfach_Irgendwa1 <faris.karge@berlin>
2024-11-29 16:35:30 +0100

    make elf.rs less dependent of the rest

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod misc;
 mod parser;
 
 use compiler::*;
+use elf::generate_elf;
 use lexer::*;
 use misc::*;
 use parser::*;
@@ -84,7 +85,7 @@ fn main() -> Result<()> {
     let mut program = Program::new();
     main.write(&mut program.text)?;
     debug!("Compiler output: {program:?}");
-    let elf = program.generate_elf();
+    let elf = generate_elf(program.text, program.data);
 
     File::create(&output).unwrap().write_all(&elf).unwrap();
     let mut permissions = metadata(&output).unwrap().permissions();
